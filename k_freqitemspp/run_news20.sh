5f4f50cd4c2210416a921ef2803de838b1d723f7
Ben Fukuzawa <115841955+BenFukuzawa@users.noreply.github.com>
2023-07-24 14:22:45 -0700

    idk

--- k_freqitemspp/run_news20.sh
+++ k_freqitemspp/run_news20.sh
@@ -9,14 +9,23 @@ make -j
 dname=News20                        # data set name
 n=19928                             # cardinality
 format=int32                        # data format: uint16, int32
-dset=../data/1/${dname}.bin         # address of data set
-ofolder=oatmeal/${dname}/           # output folder
+dset="C:/Users/benfu/k-FreqItemspp/data/1/${dname}.bin"         # address of data set
+ofolder="C:/Users/benfu/k-FreqItemspp/k_freqitemspp/oatmeal/${dname}"           # output folder
 
-k=100
-for alpha in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
-do 
-  ./kpp -n ${n} -k ${k} -a ${alpha} -f ${format} -ds ${dset} -of ${ofolder}
-done 
+# k=100
+# for alpha in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+# do 
+#   ./kpp -n ${n} -k ${k} -a ${alpha} -f ${format} -ds ${dset} -of ${ofolder}
+# done 
+
+echo "dname: ${dname}"
+echo "n: ${n}"
+echo "k: ${k}"./run
+echo "alpha: ${alpha}"
+echo "format: ${format}"
+echo "dset: ${dset}"
+echo "ofolder: ${ofolder}"
+
 
 alpha=0.2
 for k in 20 40 60 80 100 120 140 160 180 200
